32bde601b1836e2a8bf919a1d8605a07a0c8ae87
Shoaib Latif <shoaiblatif11@gmail.com>
2025-02-12 07:40:15 +0000

    Deleted unnecessary files and reorganized the project

--- app/providers.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { AuthProvider } from "@/contexts/AuthContext";
-export function Providers({
-  children
-}: {
-  children: React.ReactNode;
-}) {
-  return <AuthProvider>{children}</AuthProvider>;
-}
--- pages/index.tsx
+++
@@ -1,7 +0,0 @@
-// src/pages/index.tsx
-import React from "react";
-import { CategoriesPage } from "./CategoriesPage";
-
-export default function Home() {
-  return <CategoriesPage />
-}
